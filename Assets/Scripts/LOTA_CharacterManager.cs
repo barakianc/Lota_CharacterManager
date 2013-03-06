29b12c33642a7d4c9f8fa21963e51b1b7c7d9024
barakianc <barakianc@gmail.com>
2013-03-05 23:44:51 -0800

    added more menu items, and began work on the shop

--- Assets/Scripts/LOTA_CharacterManager.cs
+++ Assets/Scripts/LOTA_CharacterManager.cs
@@ -6,6 +6,14 @@ public class LOTA_CharacterManager : MonoBehaviour {
     private GameObject charDetails;
     private bool DisplayDets = false;
     private bool SelectChar = false;
+    private bool openShop = false;
+    private bool openSell = false;
+    private CharacterCard boughtItemhead;
+    private CharacterCard boughtItemLhand;
+    private CharacterCard boughtItemRhand;
+    private CharacterCard boughtItemChest;
+    private CharacterCard boughtItemFeet;
+    public CharacterCard[] ShopCards;
     private int selectTarget;
     public GameObject Archer;
     public GameObject Swordsman;
@@ -13,7 +21,7 @@ public class LOTA_CharacterManager : MonoBehaviour {
     public GameObject Spearman;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
@@ -23,7 +31,7 @@ public class LOTA_CharacterManager : MonoBehaviour {
 
     void OnGUI()
     {
-        if (!DisplayDets && !SelectChar)
+        if (!DisplayDets && !SelectChar && !openShop)
         {
             if (TeamCharacters[0] != null)
             {
@@ -61,7 +69,7 @@ public class LOTA_CharacterManager : MonoBehaviour {
            
             if (TeamCharacters[2] != null)
             {
-                if (GUI.Button(new Rect(25, 500, 250, 200), TeamCharacters[2].name))
+                if (GUI.Button(new Rect(25, 250, 250, 200), TeamCharacters[2].name))
                 {
                     DisplayDets = true;
                     charDetails = TeamCharacters[2];
@@ -69,7 +77,7 @@ public class LOTA_CharacterManager : MonoBehaviour {
             }
             else
             {
-                if (GUI.Button(new Rect(25, 500, 250, 200), "Add New Character"))
+                if (GUI.Button(new Rect(25, 250, 250, 200), "Add New Character"))
                 {
                     SelectChar = true;
                     selectTarget = 2;
@@ -78,7 +86,7 @@ public class LOTA_CharacterManager : MonoBehaviour {
             
             if (TeamCharacters[3] != null)
             {
-                if (GUI.Button(new Rect(500, 500, 250, 200), TeamCharacters[3].name))
+                if (GUI.Button(new Rect(500, 250, 250, 200), TeamCharacters[3].name))
                 {
                     DisplayDets = true;
                     charDetails = TeamCharacters[3];
@@ -86,14 +94,14 @@ public class LOTA_CharacterManager : MonoBehaviour {
             }
             else
             {
-                if (GUI.Button(new Rect(500, 500, 250, 200), "Add New Character"))
+                if (GUI.Button(new Rect(500, 250, 250, 200), "Add New Character"))
                 {
                     SelectChar = true;
                     selectTarget = 3;
                 }
             }
         }
-        else if (!DisplayDets && SelectChar)
+        else if (!DisplayDets && SelectChar && !openShop)
         {
             if (GUI.Button(new Rect(25, 25, 250, 200), "Archer"))
             {
@@ -105,15 +113,27 @@ public class LOTA_CharacterManager : MonoBehaviour {
                 TeamCharacters[selectTarget] = Swordsman;
                 SelectChar = false;
             }
-            if (GUI.Button(new Rect(25, 500, 250, 200), "Spearman"))
+            if (GUI.Button(new Rect(25, 250, 250, 200), "Spearman"))
             {
                 TeamCharacters[selectTarget] = Spearman;
                 SelectChar = false;
             }
-            if (GUI.Button(new Rect(500, 500, 250, 200), "Mage"))
+            if (GUI.Button(new Rect(500, 250, 250, 200), "Mage"))
             {
                 TeamCharacters[selectTarget] = Mage;
                 SelectChar = false;
+            }
+        }
+        else if (DisplayDets && !SelectChar && openShop)
+        {
+            string[] selStrings = new string[] { "Boots of Speed: +1 to movement", "Boots of Speed: +1 to movement", 
+                                                "Boots of Speed: +1 to movement", "Boots of Speed: +1 to movement" };
+            int selGridInt = 0;
+            selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 2);
+
+            if (GUI.Button(new Rect(700, 500, 75, 75), "Exit Shop"))
+            {
+                openShop = false;
             }
         }
         else
@@ -123,11 +143,25 @@ public class LOTA_CharacterManager : MonoBehaviour {
                                 + "\nDefense: " + stats.Defense.ToString() + "\nMovement: " + stats.Movement.ToString() + "\nRange: "
                                 + stats.AttackRange.ToString() + "\nHead Item: " + stats.headSlot.ItemName + "\nLeft Hand Item: " + stats.leftarmSlot.ItemName
                                 + "\nRight Hand Item: " + stats.rightarmSlot.ItemName + "\nChest Item: " + stats.chestSlot.ItemName + "\nFeet Item: "
-                                + stats.feetSlot.ItemName + "\nAbility: " + stats.Ability;
+                                + stats.feetSlot.ItemName + "\nAbility: " + stats.Ability  + "\nGold: " + stats.Gold;
 
             if (GUI.Button(new Rect(100, 50, 500, 500), details))
             {
                 DisplayDets = false;
+            }
+
+            if(GUI.Button(new Rect( 650, 50,150,150),"Add Kill!")){
+                stats.Gold += 3;
+            }
+
+            if (GUI.Button(new Rect(650, 200, 150, 150), "Buy Items"))
+            {
+                openShop = true;
+            }
+
+            if (GUI.Button(new Rect(650, 350, 150, 150), "Sell Items"))
+            {
+                openSell = true;
             }
         }
     }
